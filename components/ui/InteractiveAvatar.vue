37fb3e2303947df7fb8fa56bb9fa57e013e764fd
Valkyr97 <develloperjam.97@gmail.com>
2023-12-11 11:51:14 -0500

    refactor

--- components/ui/InteractiveAvatar.vue
+++ components/ui/InteractiveAvatar.vue
@@ -8,10 +8,9 @@ defineProps<{
 
 <template>
   <div class="flex flex-col relative group">
-    <img
+    <UtilsImg
       class="h-16 cursor-pointer w-full object-center object-contain rounded-full"
       :src="img"
-      alt=""
     />
     <div
       class="pointer-events-none z-10 top-0 -translate-y-full -my-1 p-2 absolute opacity-0 rounded-sm group-hover:opacity-100 flex transition flex-col bg-gray-700 text-white"
@@ -19,7 +18,10 @@ defineProps<{
       <h3 class="font-medium capitalize text-lg whitespace-nowrap">
         {{ title }}
       </h3>
-      <span v-if="subtitle" class="-my-1 whitespace-nowrap capitalize text-opacity-80">
+      <span
+        v-if="subtitle"
+        class="-my-1 whitespace-nowrap capitalize text-opacity-80"
+      >
         {{ subtitle }}
       </span>
     </div>
